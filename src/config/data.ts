4e502ca8ec029cad908adb244a624380e131b1b5
aswin-web <ashvinachu097@gmail.com>
2025-02-27 23:19:27 +0530

    Fix

--- src/config/data.ts
+++ src/config/data.ts
@@ -12,6 +12,7 @@ export const days = [
 export const excercise = [
   {
     dayName: "Monday",
+    dayOrgan:"Chest",
     excercises: [
       {
         video_url:
@@ -19,57 +20,174 @@ export const excercise = [
         set: 2,
         reps: "10-15/  2 failure Min 25",
         name: " Incline Smith Machine /Incline BB Pench press ",
-        type: "chest",
+        type: "Chest",
       },
       {
-        video_url: "https://youtu.be/AduT4Eq-iP0?si=7RxSgIYBQEQ-JAoD",
+        video_url:
+          "https://www.youtube.com/embed/AduT4Eq-iP0?si=2kUAn2SbBfopFPh9",
         set: 2,
         reps: "10-15/  2 failure Min 25",
         name: "  Dumbbell Flat Press",
-        type: "chest",
+        type: "Chest",
       },
       {
-        video_url: "https://youtu.be/eGjt4lk6g34?si=IYmfTLVOMaNRSUcR",
+        video_url:
+          "https://www.youtube.com/embed/eGjt4lk6g34?si=aRTHhF0yOO-nI0br",
         set: 3,
         reps: "10-15",
         name: " Pec Deck Flies ",
-        type: "chest",
+        type: "Chest",
       },
       {
-        video_url: "https://youtu.be/2jFFCy8JBU8?si=dXYfcXriSRJyT5ru",
-        set: 2,
+        video_url:
+          "https://www.youtube.com/embed/AduT4Eq-iP0?si=2kUAn2SbBfopFPh9",
+        set: 3,
         reps: "failure min 25 ",
-        name: "",
-        type: "chest",
+        name: "Dumbbell Lateral Raises",
+        type: "Shoulders",
       },
       {
-        video_url: "https://youtu.be/2jFFCy8JBU8?si=dXYfcXriSRJyT5ru",
-        set: 2,
-        reps: "10-15/  2 failure Min 25",
-        name: " Incline Smith Machine /Incline BB Pench press ",
-        type: "chest",
+        video_url:
+          "https://www.youtube.com/embed/Z5FA9aq3L6A?si=013tV_iPHIj-yqN0",
+        set: 3,
+        reps: "failure min 25 ",
+        name: "  Cable Lateral Raises ",
+        type: "Shoulders",
       },
       {
-        video_url: "https://youtu.be/2jFFCy8JBU8?si=dXYfcXriSRJyT5ru",
-        set: 2,
-        reps: "10-15/  2 failure Min 25",
-        name: " Incline Smith Machine /Incline BB Pench press ",
-        type: "chest",
+        video_url: "https://www.youtube.com/embed/2feuVZi1xx4?si=C1FNDXrFS0l8V91p",
+        set: 3,
+        reps: "10-15",
+        name: "  Cross Body Tricep Extension/Bench dips ",
+        type: "Triceps",
       },
       {
-        video_url: "https://youtu.be/2jFFCy8JBU8?si=dXYfcXriSRJyT5ru",
-        set: 2,
-        reps: "10-15/  2 failure Min 25",
-        name: " Incline Smith Machine /Incline BB Pench press ",
-        type: "chest",
+        video_url: "https://www.youtube.com/embed/9wxRhONFsRA?si=-0KDwoFbaJnoyli3",
+        set: 3,
+        reps: "10-15",
+        name: "  Katana Extension/overhead tricep extensions ",
+        type: "Triceps",
+      }
+    ],
+  },
+  {
+    dayName: "Tuesday",
+    dayOrgan:"Back",
+    excercises: [
+      {
+        video_url:
+          "https://www.youtube.com/embed/JGeRYIZdojU?si=V5d6BFkSMALD2jr9",
+        set: 3,
+        reps: "10-15",
+        name: "   Lat-Pull-down ",
+        type: "Back",
       },
       {
-        video_url: "https://youtu.be/2jFFCy8JBU8?si=dXYfcXriSRJyT5ru",
-        set: 2,
+        video_url:
+          "https://www.youtube.com/embed/JGeRYIZdojU?si=znmeNWi29IJZDAxs",
+        set: 3,
         reps: "10-15/  2 failure Min 25",
-        name: " Incline Smith Machine /Incline BB Pench press ",
-        type: "chest",
+        name: " T-Bar Row",
+        type: "Back",
       },
+      {
+        video_url:
+          "https://www.youtube.com/embed/lJoozxC0Rns?si=yEHGrka9wnbaBlB_",
+        set: 3,
+        reps: "10-15",
+        name: " Cable Row (close grip) ",
+        type: "Back",
+      },
+      {
+        video_url:
+          "https://www.youtube.com/embed/Y59M5fXn8bs?si=v6uCt9Y2uWnsOKCR",
+        set: 3,
+        reps: "failure min 25 ",
+        name: " Lat Pullover",
+        type: "Back",
+      },
+      {
+        video_url:
+          "https://www.youtube.com/embed/Y59M5fXn8bs?si=SlBHj_SfVpY9WeeV",
+        set: 3,
+        reps: "failure min 25 ",
+        name: "  Reverse Pec Deck Fly ",
+        type: "Rear Delts",
+      },
+      {
+        video_url: "https://www.youtube.com/embed/Zbs3ko8ycyg?si=-wmOng-bsPHm-rXT",
+        set: 2,
+        reps: "10-15",
+        name: "  Preacher Curl ",
+        type: "Biceps",
+      },
+      {
+        video_url: "https://www.youtube.com/embed/B4RznoFvTl4?si=f5nIvRxkMePmNDVI",
+        set: 2,
+        reps: "10-15",
+        name: "  Hammer Curl DB ",
+        type: "Biceps",
+      }
+    ],
+  },
+  {
+    dayName: "Wednesday",
+    dayOrgan:"Quads",
+    excercises: [
+      {
+        video_url:
+          "https://www.youtube.com/embed/JGeRYIZdojU?si=V5d6BFkSMALD2jr9",
+        set: 3,
+        reps: "10-15",
+        name: "    Hack Squat/Smith Squat/Goblet squat ",
+        type: "Quads",
+      },
+      {
+        video_url:
+          "https://www.youtube.com/embed/JGeRYIZdojU?si=znmeNWi29IJZDAxs",
+        set: 3,
+        reps: "10-15/  2 failure Min 25",
+        name: "  Leg Press",
+        type: "Back",
+      },
+      {
+        video_url:
+          "https://www.youtube.com/embed/lJoozxC0Rns?si=yEHGrka9wnbaBlB_",
+        set: 3,
+        reps: "10-15",
+        name: "  Leg Extension/Bulgarian split squats ",
+        type: "Back",
+      },
+      {
+        video_url:
+          "https://www.youtube.com/embed/Y59M5fXn8bs?si=v6uCt9Y2uWnsOKCR",
+        set: 3,
+        reps: "failure min 25 ",
+        name: " Lat Pullover",
+        type: "Back",
+      },
+      {
+        video_url:
+          "https://www.youtube.com/embed/Y59M5fXn8bs?si=SlBHj_SfVpY9WeeV",
+        set: 3,
+        reps: "failure min 25 ",
+        name: "  Reverse Pec Deck Fly ",
+        type: "Rear Delts",
+      },
+      {
+        video_url: "https://www.youtube.com/embed/Zbs3ko8ycyg?si=-wmOng-bsPHm-rXT",
+        set: 2,
+        reps: "10-15",
+        name: "  Preacher Curl ",
+        type: "Biceps",
+      },
+      {
+        video_url: "https://www.youtube.com/embed/B4RznoFvTl4?si=f5nIvRxkMePmNDVI",
+        set: 2,
+        reps: "10-15",
+        name: "  Hammer Curl DB ",
+        type: "Biceps",
+      }
     ],
   },
 ];
